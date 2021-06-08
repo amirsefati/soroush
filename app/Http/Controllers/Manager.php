1d001c22525dcd8a4d53de483e84a320753d74e1
soroush <soroushazizzadeh@gmail.com>
2021-06-09 00:18:47 +0430

    modir -moshaver performance table -add moshaver

--- app/Http/Controllers/Manager.php
+++ app/Http/Controllers/Manager.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\File;
+use App\Models\RangeMoshaver;
 use App\Models\Report;
 use App\Models\User;
 use App\Models\Work;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class Manager extends Controller
 {
@@ -36,6 +38,52 @@ class Manager extends Controller
         }
         return $report;
 
+    }
+
+    public function add_moshaver(){
+        return view('modir.add_moshaver');
+    }
+
+    public function add_moshaver_post(Request $request){
+
+        $space_200 = 0;
+        $space_200_350 = 0;
+        $space_350 = 0;
+        foreach($request->space as $space){
+            if ($space == 1){
+                $space_200 = 1;
+            }
+
+            if ($space == 2){
+                $space_200_350 = 1;
+            }
+
+            if ($space == 3){
+                $space_350 = 1;
+            }
+        }
+
+        $user = User::create([
+            "name" => $request->name,
+            "phone" => $request->phone,
+            "password" => $request->password,
+            "level" => 2,
+            "userid_inter" => Auth::user()->id,
+        ]);
+
+        RangeMoshaver::create([
+            "user_id" => $user->id,
+            "seller" => $request->seller == 'on' ? 1 : 0,
+            "renter" => $request->renter == 'on' ? 1 : 0,
+            "kolangi" => $request->kolangi == 'on' ? 1 : 0,
+            "tejari" => $request->tejari == 'on' ? 1 : 0,
+            "mostaghelat" => $request->mostaghelat == 'on' ? 1 : 0,
+            "space_200" => $space_200,
+            "space_200_350" => $space_200_350,
+            "space_350" => $space_350
+        ]);
+
+        return back();
     }
 }
 
